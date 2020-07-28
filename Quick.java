9bff3293ccd16231b8e886c97013dbaacd56ee03
zeruel91 <67663991+zeruel91@users.noreply.github.com>
2020-07-28 16:55:13 +0900

    Create Quick.java

---
+++ Quick.java
@@ -0,0 +1,50 @@
+package JAVA;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class Quick{
+    public static void main(String[] args){
+
+        Integer[] array = {30, 50, 7, 40, 88, 15, 44, 55, 22,
+        33, 77, 88, 11, 66, 1, 85};
+        ArrayList<Integer> aList = new ArrayList<Integer>();
+        aList.addAll(Arrays.asList(array));
+
+        aList = (ArrayList<Integer>) quicksort(aList);
+        out.print(aList.toString());
+    }
+
+    
+    public static <T extends Comparable<? super T>> List<T> 
+    quicksort(List<T> list) {
+
+        if (list.size() <=1) return list;
+        int pivot =list.size() / 2;
+        List<T> a = new ArrayList<T>();
+        List<T> b = new ArrayList<T>();
+        int c = 0;
+        for (T number : list){
+            if (list.get(pivot).compareTo(number) < 0)
+            b.add(number);
+            else if (list.get(pivot).compareTo(number) > 0)
+                a.add(number);
+            else
+                c++;
+        }
+        a = quicksort(a);
+        for (int i = 0; i < c; i ++)
+            a.add(list.get(pivot));
+
+            b = quicksort(b);
+
+            List<T> sorted = new ArrayList<T>();
+            sorted.addAll(a);
+            sorted.addAll(b);
+            return sorted;        
+    
+    }
+
+}
